5bb06a5972cb8b3e1d8a8168a98e63f0f59ced8a
TJ VanToll <tj.vantoll@gmail.com>
2013-12-02 16:05:30 -0500

    Adding a more realistic test.

--- QUnit Tester/tests/tests.js
+++ QUnit Tester/tests/tests.js
@@ -1,7 +1,16 @@
 test( "Test that passes", function() {
-    ok( true );
-});
+	var repos = $( "<ul id='repos'></ul>" ).appendTo( "#qunit-fixture" ),
+		savedGetJSON = $.getJSON;
 
-test( "Equality tests", function() {
-	ok( true ); 
+	$.getJSON = function() {
+		return $.Deferred().resolve([
+			{ name: "jQuery" },
+			{ name: "jQuery UI" }
+		]);
+	};
+
+	app.loadRepos();
+	equal( repos.find( "li" ).length, 2 );
+
+	$.getJSON = savedGetJSON;
 });
