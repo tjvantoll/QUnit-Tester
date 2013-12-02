ffc9db612f37a8e7c2e7559ccbf3fb2546563b55
TJ VanToll <tj.vantoll@gmail.com>
2013-12-02 14:39:24 -0500

    Setting up a grunt QUnit test runner.

---
+++ QUnit Tester/Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function( grunt ) {
+	grunt.initConfig({
+		qunit: {
+			all: [ "tests/*.html" ]
+		}
+	});
+
+	grunt.loadNpmTasks( "grunt-contrib-qunit" );
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+Test project for running a [QUnit](http://qunitjs.com/) test suite in a [PhoneGap](http://phonegap.com/)/[Cordova](http://cordova.apache.org/) app.
